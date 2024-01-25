a99f7a4e737342287d05e0c8eb46c11860b2bb58
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-01-25 23:04:07 +0300

    Схемка рисуется

--- common.js
+++ common.js
@@ -1,5 +1,8 @@
 export function getScale(width, height, nodes, offset) {
-	let minX, maxX, minY, maxY = 0;
+	let minX = 0,
+		maxX = 0,
+		minY = 0,
+		maxY = 0;
 	nodes.forEach(node => {
 		minX = Math.min(minX, node[0]);
 		maxX = Math.max(maxX, node[0]);
@@ -9,4 +12,14 @@ export function getScale(width, height, nodes, offset) {
 	let scaleX = (width - 2*offset) / (maxX - minX);
 	let scaleY = (height - 2*offset) / (maxY - minY);
 	return Math.min(scaleX, scaleY);
+}
+
+export function drawBar(ctx, start, end, color='#fff') {
+	ctx.beginPath();
+	ctx.strokeStyle = color;
+	ctx.lineWidth = 20;
+	ctx.moveTo(start[0], start[1]);
+	ctx.lineTo(end[0], end[1]);
+	ctx.stroke();
+	ctx.closePath();
 }
--- renderer.js
+++ renderer.js
@@ -1,11 +1,17 @@
-import { getScale } from './common.js'
-import { schema } from './schema.js';
+import { getScale, drawBar } from './common.js'
+import {nodes, bars} from './schema.js';
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const offset = 100;
 
-let scale = getScale(canvas.width, canvas.height, schema.nodes, offset);
+let scale = getScale(canvas.width, canvas.height, nodes, offset);
 
-ctx.scale(scale, scale);
-ctx.translate(offset, offset);
+ctx.translate(offset, (canvas.height-offset));
+ctx.scale(scale, -scale);
+ctx.save();
+
+bars.forEach(bar => {
+	drawBar(ctx, nodes[bar[0]], nodes[bar[1]]);
+});
+
