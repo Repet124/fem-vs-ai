4b3dcd48e9be0a4028d6434fd74d2440f16ada4d
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-02-04 01:17:04 +0300

    Переосмыслен билдер датасетов

--- dataset/datasetBuilder.js
+++ dataset/datasetBuilder.js
@@ -9,6 +9,8 @@ class DatasetBuilder {
 
 	constructor(shBuilder) {
 		this.schemesBuilder = shBuilder || new SchemesBuilder();
+
+		this.sources = null;
 		this.dataset = {
 			tensors: null,
 			translations: null
@@ -16,29 +18,56 @@ class DatasetBuilder {
 		this.logger = new Logger('DatasetBuilder');
 	}
 
-	createDataset(count=20) {
-		this.logger.info('Начато формирования датасетов');
-		this.logger.bench('dataset');
-
-		this.dataset.tensors = Array(count);
-		this.dataset.translations = Array(count);
-
-		this.schemesBuilder.getSchemes(count).forEach((schema,i) => {
-			var input = new InputBuilder(schema);
-			var output = new OutputBuilder(calc(schema));
-
-			this.dataset.tensors[i] = {
+	getTensorsDataset() {
+		if (!this.dataset.tensors) {
+			this.sourcesCheck();
+			this.logger.info('Формирования датасета для усилий');
+			this.dataset.tensors = this.sources.map(source => ({
 				input: input.getBarsInput(),
 				output: output.getForces()
-			}
-			this.dataset.translations[i] = {
+			}));
+			this.logger.info('Формирования датасета для усилий завершено');
+		}
+		return this.dataset.tensors;
+	}
+
+	getTranslatioinsDataset() {
+		if (!this.dataset.translations) {
+			this.sourcesCheck();
+			this.logger.info('Запуск формирования датасета для перемещений');
+			this.dataset.translations = this.sources.map(source => ({
 				input: input.getNodesInput(),
 				output: output.getTranslations()
-			}
-		});
+			}));
+			this.logger.info('Формирования датасета для перемещений завершено');
+		}
+		return this.dataset.translations;
+	}
 
-		this.logger.success('Датасеты созданы');
-		this.logger.bench('dataset');
+	sourcesCheck() {
+		if (!this.sources) {
+			throw Error('Невозможно создать датасет - нет исходных данных');
+		}
+	}
+
+	buildSources(count) {
+		if (!Number.isInteger(count)) {
+			throw new Error('Не указано количество расчётных схем');
+		}
+
+		this.dataset.tensors = null;
+		this.dataset.translations = null;
+
+		this.logger.success('Начато формирования данных для датасета');
+		this.logger.bench('data');
+
+		this.sources = this.schemesBuilder.getSchemes(count).forEach(schema => ({
+			inputs: new InputBuilder(schema),
+			outputs: new OutputBuilder(calc(schema))
+		}));
+
+		this.logger.success('Данные для датасета сформированы');
+		this.logger.bench('data');
 	}
 
 	saveTensorsDataset(fileName='./tensors-dataset.json') {
@@ -56,6 +85,10 @@ class DatasetBuilder {
 	}
 
 	#save(datasetName, fileName) {
+		if (!this.dataset[datasetName]) {
+			this.logger.err('Невозможно сохранить датасет ' + datasetName + ' - датасет не сформирован');
+			return;
+		}
 		fs.writeFileSync(fileName, JSON.stringify(this.dataset[datasetName]), 'utf8');
 		this.logger.success('Датасет сохранён в ' + fileName);
 	}
