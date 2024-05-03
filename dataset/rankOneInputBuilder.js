76aed0ec9141ae48d5b3d7e0f518be174a0370a1
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-05-03 19:18:10 +0300

    only forces in dataset

--- dataset/rankOneInputBuilder.js
+++ dataset/rankOneInputBuilder.js
@@ -24,14 +24,29 @@ module.exports = class InputBuilder {
 		// пример: в узле с нагрузками по x,y [-1, 2], входные нейроны будут [0,1,2,0], до нормализации
 
 		// const cords = this.schema.nodes.map(node => [node[0], node[1]]).flat();
-		const forces = this.schema.forces.flat();
-		const reactions = this.schema.nodes.map(node => [node[2], node[3]]).flat().map(R => R === 0 ? 1 : 0);
 
-		this.dataset = [
-			// ...(cords.concat(forces).separateNegative().normalize()),
-			...(forces.separateNegative().normalize()),
-			...reactions,
-		];
+		this.dataset = this.schema.forces
+			.map((force,i) => {
+				force = [...force];
+				if (this.schema.nodes[i][2] === 0) {
+					force.shift();
+				} else if(this.schema.nodes[i][3] === 0) {
+					force.pop();
+				}
+				return force;
+			})
+			.flat()
+			.separateNegative()
+			.normalize();
+
+		// const forces = this.schema.forces.flat();
+		// const reactions = this.schema.nodes.map(node => [node[2], node[3]]).flat().map(R => R === 0 ? 1 : 0);
+
+		// this.dataset = [
+		// 	// ...(cords.concat(forces).separateNegative().normalize()),
+		// 	...(forces.separateNegative().normalize()),
+		// 	...reactions,
+		// ];
 
 		this.logger.success('Формирование матрицы исходных данных для датасета звершено')
 		this.logger.bench('form');
