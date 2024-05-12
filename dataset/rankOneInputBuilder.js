d441029715a811e20183790946d71aac5f994ccd
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-05-12 16:20:58 +0300

    raw forces to input layer

--- dataset/rankOneInputBuilder.js
+++ dataset/rankOneInputBuilder.js
@@ -24,33 +24,7 @@ module.exports = class InputBuilder {
 		// пример: в узле с нагрузками по x,y [-1, 2], входные нейроны будут [0,1,2,0], до нормализации
 
 		// const cords = this.schema.nodes.map(node => [node[0], node[1]]).flat();
-
-		this.dataset = this.schema.forces
-			.map((force,i) => {
-				force = [...force];
-				if (this.schema.nodes[i][2] === 0) {
-					force.shift();
-				} else if(this.schema.nodes[i][3] === 0) {
-					force.pop();
-				}
-				return force;
-			})
-			.flat()
-			.separateNegative();
-
-		// let max = Math.max(...this.dataset);
-		let max = 20;
-		// let maxForce = 20*this.schema.forces.length/4;
-		this.dataset = this.dataset.map(force => force/max);
-
-		// const forces = this.schema.forces.flat();
-		// const reactions = this.schema.nodes.map(node => [node[2], node[3]]).flat().map(R => R === 0 ? 1 : 0);
-
-		// this.dataset = [
-		// 	// ...(cords.concat(forces).separateNegative().normalize()),
-		// 	...(forces.separateNegative().normalize()),
-		// 	...reactions,
-		// ];
+		this.dataset = this.schema.forces.flat();
 
 		this.logger.success('Формирование матрицы исходных данных для датасета звершено')
 		this.logger.bench('form');
