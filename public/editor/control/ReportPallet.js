f442116de981e1b2ff20dead0193cba25914bf4a
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-06-21 21:22:57 +0300

    absalut changes reporting

--- public/editor/control/ReportPallet.js
+++ public/editor/control/ReportPallet.js
@@ -24,6 +24,10 @@ class ReportPallet {
 		var fem = this.#femSchema.bars.map(bar => bar[3]);
 		var ai = this.#aiSchema.bars.map(bar => bar[3]);
 
+		var absDelta = fem.map((f,i) => Number(Math.abs(f-ai[i]).toFixed(2)));
+		var absMaxIndex = absDelta.indexOf(Math.max(...absDelta))
+		var absMinIndex = absDelta.indexOf(Math.min(...absDelta))
+
 		var delta = fem.map((f,i) => {
 			var val = getDelta(f, ai[i]);
 			// if (val > 1) {
@@ -61,6 +65,12 @@ class ReportPallet {
 		this.#buildPercentRow('strech', getDelta(fem[maxIndex], ai[maxIndex]));
 		// Отклонения максимального сжимающего
 		this.#buildPercentRow('compress', getDelta(fem[minIndex], ai[minIndex]));
+		// Максимальное абсолютное отклоненние
+		this.#buildReportRow('absMax', `${absDelta[absMaxIndex]}, FEM - ${fem[absMaxIndex]}, AI - ${ai[absMaxIndex]}`);
+		// Минимальное абсолютное отклоненеие
+		this.#buildReportRow('absMin', absDelta[absMinIndex]);
+		// Сред абс откл
+		this.#buildReportRow('absAverage', absDelta.reduce((accum,f) => accum+f, 0)/absDelta.length);
 	}
 
 	#buildPercentRow(spanId, val){
