bff37fa6e127ca94071eb879fc198382e4080d9f
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-07-01 01:49:33 +0300

    remove deprecated file

--- preload.js
+++ preload.js
@@ -6,7 +6,7 @@ contextBridge.exposeInMainWorld('api', {
 	sync: (schema, settings) => ipcRenderer.invoke('sync', schema, settings),
 	upload: () => ipcRenderer.invoke('upload'),
 	save: () => ipcRenderer.invoke('save'),
-	dataset: () => ipcRenderer.invoke('dataset', ),
-	train: () => ipcRenderer.invoke('train', ),
+	dataset: () => ipcRenderer.invoke('dataset'),
+	train: () => ipcRenderer.invoke('train'),
 	calc: (type) => ipcRenderer.invoke('calc', type),
 });
